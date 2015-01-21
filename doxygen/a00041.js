4ea718c2603232b6d9d0ef3f87ea031b08fdefc0
Nicuvëo <crucuny@gmail.com>
2015-01-21 20:33:58 +0100

    Update doxygen.

--- doxygen/a00041.js
+++ doxygen/a00041.js
@@ -2,6 +2,6 @@ var a00041 =
 [
     [ "clamp", "a00041.html#a550aada9728b34f662270272e6309ab9", null ],
     [ "clean", "a00041.html#a111f445ef4a851091c2e045ebb356f81", null ],
-    [ "round", "a00041.html#ac8a2405443faadde2cc09494b4e92b26", null ],
-    [ "d_mod", "a00041.html#a94969011d95c53f391a51146c947a871", null ]
+    [ "round", "a00041.html#aaa42eecbe358386e76c2e614e95fd106", null ],
+    [ "d_mod", "a00041.html#aae9f08a181d6bbb2b813fc16d7fbe8b3", null ]
 ];
